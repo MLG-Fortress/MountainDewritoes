07260e59f3b4a36f2ff1b43ad2fb210293ac4e36
RoboMWM <robomwm@users.noreply.github.com>
2016-08-28 20:00:11 -0700

    Drop a maximum of 8 experience levels
    And keep the rest on respawn

--- src/main/java/me/robomwm/MountainDewritoes/DeathListener.java
+++ src/main/java/me/robomwm/MountainDewritoes/DeathListener.java
@@ -24,6 +24,7 @@ public class DeathListener implements Listener
     Main instance;
     Random random = new Random();
     HashMap<Player, HashSet<ItemStack>> deathItems = new HashMap<>();
+    HashMap<Player, Integer> deathExp = new HashMap<>();
     DeathListener(Main iKnowIShouldntCallItMain)
     {
         instance = iKnowIShouldntCallItMain;
@@ -34,7 +35,9 @@ public class DeathListener implements Listener
     {
         final Player player = event.getEntity();
 
-        //Only drop some items (randomly determined)
+        /**
+        Only drop some items (randomly determined)
+         */
         ItemStack drop;
         List<ItemStack> drops = event.getDrops();
         Iterator<ItemStack> iterator = drops.iterator();
@@ -48,35 +51,50 @@ public class DeathListener implements Listener
         }
         deathItems.put(player, dropsToReturn);
 
+        /**
+         * Only lose 8 XP (vs. all XP on death)
+         */
+        if (player.getTotalExperience() > 8)
+            deathExp.put(player, player.getTotalExperience() - 8);
 
         //Believe it or not, the Minecraft client does not even trigger this sound on player death,
         //it just plays player_hurt, so yea...
         //Apparently, it actually triggers it for other players, just not the player who died, I guess...?
         player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_DEATH, 1.0f, 1.0f);
 
-        //Auto-respawn player if they haven't clicked respawn within the last 20 seconds
+        /**Auto-respawn player if they haven't clicked respawn within the last 5 seconds
         //Helps prevent weird client problems like client-side entity buildup or whatever,
-        //thus freezing the client or idk that's what happened to me.
+        thus freezing the client or idk that's what happened to me.
+         */
         new BukkitRunnable()
         {
             public void run()
             {
                 player.spigot().respawn();
             }
-        }.runTaskLater(instance, 400L);
+        }.runTaskLater(instance, 100L);
     }
 
     /**
-     * Give back items that were not dropped on death
+     * Give back items and exp that were not dropped on death
      */
     @EventHandler(ignoreCancelled = true, priority = EventPriority.HIGHEST)
     void onPlayerRespawn(PlayerRespawnEvent event)
     {
         Player player = event.getPlayer();
-        if (!deathItems.containsKey(player))
-            return;
+        //Items
+        if (deathItems.containsKey(player))
+        {
+            for (ItemStack drop : deathItems.get(player))
+                player.getInventory().addItem(drop);
+            deathItems.remove(player);
+        }
 
-        for (ItemStack drop : deathItems.get(player))
-            player.getInventory().addItem(drop);
+        //Experience
+        if (deathExp.containsKey(player))
+        {
+            player.setTotalExperience(deathExp.get(player));
+            deathExp.remove(player);
+        }
     }
 }
