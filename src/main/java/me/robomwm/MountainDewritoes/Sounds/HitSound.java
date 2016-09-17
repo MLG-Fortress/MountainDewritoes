5e37eeaa74c217ff62d0745adf8611280c01b892
RoboMWM <robomwm@users.noreply.github.com>
2016-09-17 13:29:48 -0700

    hitsounds and hitmarkers

---
+++ src/main/java/me/robomwm/MountainDewritoes/Sounds/HitSound.java
@@ -0,0 +1,78 @@
+package me.robomwm.MountainDewritoes.Sounds;
+
+import com.destroystokyo.paper.Title;
+import org.bukkit.ChatColor;
+import org.bukkit.Sound;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Projectile;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityCombustByEntityEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+
+/**
+ * Created by RoboMWM on 9/17/2016.
+ * TODO: add "self-damage" sounds
+ */
+public class HitSound implements Listener
+{
+    Title hitMarker;
+    Title largeHitMarker;
+    HitSound()
+    {
+        Title.Builder title = new Title.Builder();
+        title.title(" ");
+        title.subtitle(ChatColor.WHITE + "x");
+        title.fadeIn(0);
+        title.stay(2);
+        title.fadeOut(3);
+        hitMarker = title.build();
+        title.subtitle(ChatColor.WHITE + "X");
+        largeHitMarker = title.build();
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onPlayerDamage(EntityDamageByEntityEvent event)
+    {
+        handleEntityDamageEventCuzThxSpigot(event);
+    }
+
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.MONITOR)
+    void onPlayerIgniteWithArrow(EntityCombustByEntityEvent event)
+    {
+        EntityDamageByEntityEvent eventWrapper = new EntityDamageByEntityEvent(event.getCombuster(), event.getEntity(), EntityDamageEvent.DamageCause.FIRE_TICK, event.getDuration());
+        handleEntityDamageEventCuzThxSpigot(eventWrapper);
+    }
+
+    void handleEntityDamageEventCuzThxSpigot(EntityDamageByEntityEvent event)
+    {
+        Entity damager = event.getDamager();
+        //Check if attacker is a player or if damage was caused due to a projectile
+        if (damager.getType() != EntityType.PLAYER && !(damager instanceof Projectile))
+            return;
+
+        //Get the attacker
+        boolean critical;
+        Player attacker = null;
+        if (damager instanceof Projectile)
+        {
+            Projectile projectile = (Projectile)damager;
+            if (!(projectile.getShooter() instanceof Player))
+                return; //Dispenser
+            attacker = (Player)projectile.getShooter();
+        }
+        else
+            attacker = (Player)damager;
+
+        attacker.playSound(attacker.getLocation(), Sound.UI_BUTTON_CLICK, 3000000f, 1f);
+
+        if (event.getFinalDamage() < 6)
+            attacker.sendTitle(hitMarker);
+        else
+            attacker.sendTitle(largeHitMarker);
+    }
+}
