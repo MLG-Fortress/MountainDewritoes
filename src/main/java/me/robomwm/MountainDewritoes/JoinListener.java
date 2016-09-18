1a5415cce1cc7d9db0a021eb7a6a8f8f3aa1e348
RoboMWM <robomwm@users.noreply.github.com>
2016-09-18 02:55:46 -0700

    Join Listener thingy

---
+++ src/main/java/me/robomwm/MountainDewritoes/JoinListener.java
@@ -0,0 +1,44 @@
+package me.robomwm.MountainDewritoes;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.scheduler.BukkitRunnable;
+
+/**
+ * Created by RoboMWM on 9/18/2016.
+ */
+public class JoinListener implements Listener
+{
+    World spawn;
+    Location spawnLocation;
+    MountainDewritoes instance;
+    public JoinListener(MountainDewritoes mountainDewritoes)
+    {
+        instance = mountainDewritoes;
+        spawn = Bukkit.getWorld("minigames");
+        spawnLocation = new Location(spawn, -389D, 5D, -124D, 180.344f, -18.881f);
+    }
+    @EventHandler(priority = EventPriority.HIGHEST)
+    void onPlayerJoin(PlayerJoinEvent event)
+    {
+        Player player = event.getPlayer();
+        if (player.hasPermission("i.am.jailed")) //ignore jailed players
+            return;
+        if (player.getWorld() != spawn)
+            return;
+
+        new BukkitRunnable()
+        {
+            public void run()
+            {
+                player.teleport(spawnLocation);
+            }
+        }.runTaskLater(instance, 1L);
+    }
+}
