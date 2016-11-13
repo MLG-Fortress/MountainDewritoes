d8b5798f87d9191aa5f539543ed7e81ec22d8fc6
RoboMWM <robomwm@users.noreply.github.com>
2016-11-12 17:25:03 -0800

    hook into BetterTPA

--- src/main/java/me/robomwm/MountainDewritoes/TeleportingEffects.java
+++ src/main/java/me/robomwm/MountainDewritoes/TeleportingEffects.java
@@ -1,6 +1,10 @@
 package me.robomwm.MountainDewritoes;
 
+import me.clip.actionannouncer.ActionAPI;
+import me.robomwm.BetterTPA.PostTPATeleportEvent;
+import me.robomwm.BetterTPA.PreTPATeleportEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.Effect;
 import org.bukkit.Location;
 import org.bukkit.World;
@@ -10,6 +14,7 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -25,6 +30,7 @@ public class TeleportingEffects implements Listener
     World spawn = Bukkit.getWorld("minigames");
     World mall = Bukkit.getWorld("mall");
     Map<Player, Location> preTeleportingPlayers = new HashMap<>();
+    Map<Player, BukkitTask> taskThingy = new HashMap<>();
     MountainDewritoes instance;
 
     TeleportingEffects(MountainDewritoes mountainDewritoes)
@@ -36,40 +42,61 @@ public class TeleportingEffects implements Listener
     void playerWannaTeleport(PlayerCommandPreprocessEvent event)
     {
         Player player = event.getPlayer();
-        World world = player.getWorld();
-        if (world == spawn || world == mall)
-            return;
-        if (preTeleportingPlayers.containsKey(player))
-            return;
-
         String[] message = event.getMessage().split(" ");
         String command = message[0].toLowerCase();
         //I really wish EssentialsX provided an API/event for this. Am gonna make issue 4 dis now
-        if ((message.length > 1 && (command.equals("/tpa") || command.equals("/tpask") || command.equals("/warp")))
+        if ((message.length > 1 && command.equals("/warp"))
                 || (command.equals("/spawn") || command.equals("/mall") || command.equals("/back")))
         {
-            preTeleportingPlayers.put(player, player.getLocation());
-            new BukkitRunnable()
+            playTeleportEffect(player);
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR, ignoreCancelled = true)
+    void onPlayerTPA(PreTPATeleportEvent event)
+    {
+        playTeleportEffect(event.getPlayer());
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    void onPlayerTPACancel(PostTPATeleportEvent event)
+    {
+        taskThingy.remove(event.getPlayer()).cancel();
+        preTeleportingPlayers.remove(event.getPlayer());
+        if (event.isCancelled())
+            return;
+        ActionAPI.sendTimedPlayerAnnouncement(instance, event.getTarget(), event.getPlayer().getDisplayName() + ChatColor.AQUA + " teleported to you.", 5);
+    }
+
+    void playTeleportEffect(Player player)
+    {
+        if (preTeleportingPlayers.containsKey(player))
+            return;
+        preTeleportingPlayers.put(player, player.getLocation());
+        World world = player.getWorld();
+        BukkitTask task = new BukkitRunnable()
+        {
+            public void run()
             {
-                public void run()
+                try
                 {
-                    try
-                    {
-                        if (preTeleportingPlayers.get(player).distanceSquared(player.getLocation()) < 0.3D)
-                            world.playEffect(player.getLocation().add(0.0d, 1.0d, 0.0d), Effect.ENDER_SIGNAL, 0, 10);
-                        else
-                        {
-                            preTeleportingPlayers.remove(player);
-                            this.cancel();
-                        }
-                    }
-                    catch (IllegalArgumentException e)
+                    if (preTeleportingPlayers.get(player).distanceSquared(player.getLocation()) < 0.3D)
+                        world.playEffect(player.getLocation().add(0.0d, 1.0d, 0.0d), Effect.ENDER_SIGNAL, 0, 10);
+                    else
                     {
                         preTeleportingPlayers.remove(player);
                         this.cancel();
+                        taskThingy.remove(player);
                     }
                 }
-            }.runTaskTimer(instance, 10L, 10L);
-        }
+                catch (IllegalArgumentException e)
+                {
+                    preTeleportingPlayers.remove(player);
+                    this.cancel();
+                    taskThingy.remove(player);
+                }
+            }
+        }.runTaskTimer(instance, 10L, 10L);
+        taskThingy.put(player, task);
     }
 }
