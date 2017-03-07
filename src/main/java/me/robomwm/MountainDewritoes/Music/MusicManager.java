ef97c02cfe94cda2cc40b42e64ec61338b7a24bf
RoboMWM <robomwm@users.noreply.github.com>
2017-03-06 18:15:37 -0800

    infuriating

--- src/main/java/me/robomwm/MountainDewritoes/Music/MusicManager.java
+++ src/main/java/me/robomwm/MountainDewritoes/Music/MusicManager.java
@@ -34,6 +34,7 @@ class MusicManager
             musicSection.createSection("mall").set("http://localhost/test/adfsfd_lkji.mp3", 20);
             instance.saveConfig();
             instance.getLogger().info("saved blank config");
+            instance.reloadConfig();
         }
 
         for (String sectionName : musicSection.getKeys(false))
