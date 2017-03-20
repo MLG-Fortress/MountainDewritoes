5690f9109bd3fe2fc41a927a841da1c195f78742
RoboMWM <robomwm@users.noreply.github.com>
2017-03-20 16:46:26 -0700

    make crackshot use our new and improved tntprimed

--- src/main/java/me/robomwm/MountainDewritoes/TNTSourcer.java
+++ src/main/java/me/robomwm/MountainDewritoes/TNTSourcer.java
@@ -1,19 +1,17 @@
 package me.robomwm.MountainDewritoes;
 
-import me.robomwm.usefulutil.UsefulUtil;
-import org.bukkit.GameMode;
+import me.robomwm.usefulnmsutil.UsefulNMSUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.entity.TNTPrimed;
 import org.bukkit.event.EventHandler;
-import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.entity.EntityExplodeEvent;
+import org.bukkit.event.entity.ExplosionPrimeEvent;
 import org.bukkit.metadata.FixedMetadataValue;
 import org.bukkit.metadata.Metadatable;
-import org.bukkit.scheduler.BukkitRunnable;
 
 /**
  * Created on 3/20/2017.
@@ -32,17 +30,6 @@ public class TNTSourcer implements Listener
         instance.registerListener(this);
     }
 
-    private void cleanup(Metadatable thing)
-    {
-        new BukkitRunnable()
-        {
-            public void run()
-            {
-                thing.removeMetadata("SOURCE", instance);
-            }
-        }.runTaskLater(instance, 20L); //in case some plugins want to do delayed stuff I guess...
-    }
-
     //Back in my day, punching TNT ignited TNT you youngens
     //Used with some inspiration from CTBPunchTNT, which seems to have disappeared?
     @EventHandler(ignoreCancelled = true)
@@ -54,17 +41,50 @@ public class TNTSourcer implements Listener
             tnt.setX(tnt.getX() + 0.5D);
             tnt.setY(tnt.getY() + 0.6D);
             tnt.setZ(tnt.getZ() + 0.5D);
-            UsefulUtil.spawnSourcedTNTPrimed(tnt, event.getPlayer());
+            UsefulNMSUtil.spawnSourcedTNTPrimed(tnt, event.getPlayer());
             //entity.setMetadata("SOURCE", new FixedMetadataValue(instance, event.getPlayer()));
             event.setCancelled(true);
             event.getBlock().setType(Material.AIR);
         }
     }
 
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onEntityExplode(EntityExplodeEvent event)
+    @EventHandler
+    private void onCrackShotExplosion(ExplosionPrimeEvent event)
     {
-        if (event.getEntity().hasMetadata("SOURCE"))
-            event.getEntity().removeMetadata("SOURCE", instance);
+        if (event.getEntityType() != EntityType.PRIMED_TNT)
+            return;
+        if (!event.getEntity().hasMetadata("CS_Label"))
+            return;
+
+        Player player = null;
+        TNTPrimed tnt = (TNTPrimed)event.getEntity();
+        if (tnt.hasMetadata("CS_pName"))
+            player = instance.getServer().getPlayer(tnt.getMetadata("CS_pName").get(0).asString());
+        TNTPrimed betterTNT = UsefulNMSUtil.spawnSourcedTNTPrimed(tnt.getLocation(), player);
+
+        //Copy over metadata
+        copyMetadata(tnt, betterTNT, "CS_Label");
+        copyMetadata(tnt, betterTNT, "CS_potex");
+        copyMetadata(tnt, betterTNT, "C4_Friendly");
+        copyMetadata(tnt, betterTNT, "nullify");
+        copyMetadata(tnt, betterTNT, "CS_nodam");
+        copyMetadata(tnt, betterTNT, "CS_pName");
+        copyMetadata(tnt, betterTNT, "CS_ffcheck");
+        copyMetadata(tnt, betterTNT, "0wner_nodam");
+
+        //Copy entity attributes
+        betterTNT.setYield(tnt.getYield());
+        betterTNT.setIsIncendiary(tnt.isIncendiary());
+        betterTNT.setFuseTicks(tnt.getFuseTicks());
+
+        //Cancel and remove original
+        event.setCancelled(true);
+        event.getEntity().remove();
+    }
+
+    void copyMetadata(Metadatable from, Metadatable to, String key)
+    {
+        if (from.hasMetadata(key))
+            to.setMetadata(key, new FixedMetadataValue(instance, from.getMetadata(key).get(0).value()));
     }
 }
