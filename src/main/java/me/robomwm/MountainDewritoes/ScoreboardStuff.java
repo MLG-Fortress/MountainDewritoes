7e6c17c2e0d9cb7923229f93842adf9d96460c21
RoboMWM <robomwm@users.noreply.github.com>
2017-08-15 11:30:19 -0700

    prepare for scoreboardstats

--- src/main/java/me/robomwm/MountainDewritoes/ScoreboardStuff.java
+++ src/main/java/me/robomwm/MountainDewritoes/ScoreboardStuff.java
@@ -2,6 +2,9 @@ package me.robomwm.MountainDewritoes;
 
 import net.milkbowl.vault.economy.Economy;
 import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
 
@@ -13,7 +16,7 @@ import java.util.Map;
  *
  * @author RoboMWM
  */
-public class ScoreboardStuff
+public class ScoreboardStuff implements Listener
 {
     private Map<Player, Double> oldBalances = new HashMap<>();
     public ScoreboardStuff(JavaPlugin plugin, Economy economy)
@@ -23,8 +26,31 @@ public class ScoreboardStuff
             @Override
             public void run()
             {
+                for (Player player : plugin.getServer().getOnlinePlayers())
+                {
+                    if (!oldBalances.containsKey(player))
+                        oldBalances.put(player, economy.getBalance(player));
+                    double oldBalance = oldBalances.get(player);
+                    double newBalance = economy.getBalance(player);
+                    double difference = newBalance - oldBalance;
+                    if (difference != 0)
+                    {
+                        if (difference > 0)
+                        {
 
+                        }
+                        else if (difference < 0)
+                        {
+
+                        }
+                    }
+                }
             }
         }.runTaskTimer(plugin, 300L, 200L);
+    }
+
+    private void onQuit(PlayerQuitEvent event)
+    {
+        oldBalances.remove(event.getPlayer());
     }
 }
