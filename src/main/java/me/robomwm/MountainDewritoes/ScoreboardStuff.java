30802d41cc016ba4f93faf5297434587b91ef3ad
RoboMWM <robomwm@users.noreply.github.com>
2017-08-15 15:18:11 -0700

    zz

--- src/main/java/me/robomwm/MountainDewritoes/ScoreboardStuff.java
+++ src/main/java/me/robomwm/MountainDewritoes/ScoreboardStuff.java
@@ -55,12 +55,12 @@ public class ScoreboardStuff implements Listener
 
                         if (difference > 0)
                         {
-                            sbManager.update(player, "Credit: + " + ChatColor.GREEN  + economy.format(difference), 1);
+                            sbManager.update(player, "Credit:  " + ChatColor.GREEN + "+" + economy.format(difference), 1);
                             player.playSound(player.getLocation(), "fortress.credit", SoundCategory.PLAYERS, 300000f, 1.0f);
                         }
                         else if (difference < 0)
                         {
-                            sbManager.update(player, "Debit: - " + ChatColor.RED + economy.format(-difference), 1);
+                            sbManager.update(player, "Debit:   " + ChatColor.RED + "-" + economy.format(-difference), 1);
                             player.playSound(player.getLocation(), "fortress.debit", SoundCategory.PLAYERS, 300000f, 1.0f);
                         }
 
