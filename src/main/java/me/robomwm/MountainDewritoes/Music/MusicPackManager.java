6e790b7bc28f42b463c8b0fa0524637445359437
RoboMWM <robomwm@users.noreply.github.com>
2017-08-16 13:45:01 -0700

    finish up the prior commit

--- src/main/java/me/robomwm/MountainDewritoes/Music/MusicPackManager.java
+++ src/main/java/me/robomwm/MountainDewritoes/Music/MusicPackManager.java
@@ -37,6 +37,9 @@ class MusicPackManager
         add("wiishopthemejazzcover", 120+120+18);
         add("wiishopthing", 120+17);
         add("yeoldetemmieshop", 120+10);
+        
+        category = "minediamonds";
+        add("minediamonds", 20);
     }
 
     private void add(String soundName, long durationInSeconds)
