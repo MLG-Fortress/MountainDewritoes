8e247407222b251195951739eaed66437b9a88ad
Yoreni <yoreni@icloud.com>
2017-08-18 20:44:47 +0100

    players get money when they lvl up

--- src/main/java/me/robomwm/MountainDewritoes/LevelingProgression.java
+++ src/main/java/me/robomwm/MountainDewritoes/LevelingProgression.java
@@ -20,9 +20,12 @@ import org.bukkit.plugin.java.JavaPlugin;
  */
 public class LevelingProgression implements Listener
 {
+	Economy economy;
+	
     public LevelingProgression(JavaPlugin plugin)
     {
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
+		this.economy = economy;
     }
 
     /**
@@ -55,15 +58,26 @@ public class LevelingProgression implements Listener
     private void levelChangeEvent(PlayerExpChangeEvent event)
     {
         //TODO: track exp (so players don't lose any)
-        if (event.getSource() == null)
-            return;
+        if (event.getSource() == null) return;
         Player player = event.getPlayer();
 
-        int nextLevel = SetExpFix.getExpUntilNextLevel(player);
+        int lvl = player.getLevel();
+		int xp = 0;
+		if(lvl <= 16) xp = 2 * lvl + 7;
+		else if(lvl >= 17 && lvl <= 31) xp = 5 * lvl - 38;
+		else if(lvl >= 32) xp = 9 * lvl - 158; 
 
-        if (event.getAmount() >= nextLevel)
+        if (player.getExp() + Double.parseDouble(args[0]) >= xp) //change how it checks for lvl up cos it triggersed when the player dont have the xp to lvl up
         {
-            player.sendActionBar("You leveled up(?) to level " + player.getLevel() + 1);
+			double money = 0;
+			//the amount of money is rewarded is based on how much xp it takes to lvl up and level the player is on
+			if(lvl <= 30) money = xp * 2.5;
+			else if (lvl >= 31 && lvl <= 50) money = xp * 3.75;
+			else if (lvl >= 51 && lvl <= 75) money = xp * 4.5;
+			else if (lvl >= 76 && lvl <= 110) money = xp * 6;
+			else money = xp * 7.5;
+			eco.depositPlayer(player,money);
+			player.sendActionBar(ChatColor.GREEN + "You gained " + eco.format(money) + " for leveling up to level " + (player.getLevel() + 1));
         }
     }
 }
