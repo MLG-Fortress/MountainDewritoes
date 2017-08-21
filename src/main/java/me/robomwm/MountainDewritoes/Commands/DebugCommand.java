25004052f07add0fd286ee09e9507183405848c1
RoboMWM <robomwm@users.noreply.github.com>
2017-08-20 20:49:39 -0700

    add reloadBlocks debug command

--- src/main/java/me/robomwm/MountainDewritoes/Commands/DebugCommand.java
+++ src/main/java/me/robomwm/MountainDewritoes/Commands/DebugCommand.java
@@ -4,6 +4,8 @@ import me.robomwm.MountainDewritoes.Music.MusicThing;
 import me.robomwm.MountainDewritoes.SimpleClansListener;
 import net.sacredlabyrinth.phaed.simpleclans.managers.ClanManager;
 import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -25,12 +27,34 @@ public class DebugCommand implements CommandExecutor
 
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args)
     {
-        if (!sender.isOp())
-            return false;
+        Player player = null;
+        if (sender instanceof Player)
+            player = (Player)sender;
 
         //1 arg//
         if (args.length < 1)
             return false;
+
+        if (args[0].equalsIgnoreCase("reloadBlocks"))
+        {
+            assert player != null;
+            Location location = player.getLocation();
+
+            for (int x = 0; x <= 10; x++)
+            {
+                location.add(x, 0, 0);
+                for (int y = 0; y <= 10; y++)
+                {
+                    location.add(0, y, 0);
+                    for (int z = 0; z <= 10; z++)
+                    {
+                        location.add(0, 0, z);
+                        Block block = location.getBlock();
+                        player.sendBlockChange(location, block.getType(), block.getData());
+                    }
+                }
+            }
+        }
 
         //2 args//
         if (args.length < 2)
@@ -41,7 +65,7 @@ public class DebugCommand implements CommandExecutor
         if (target == null)
             return false;
 
-        if (args[0].equalsIgnoreCase("music"))
+        if (sender.isOp() && args[0].equalsIgnoreCase("music"))
         {
 
             if (!target.hasMetadata("MD_LISTENING"))
