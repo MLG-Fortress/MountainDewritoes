389eaf2c691aa76c1b8c04942eb207ef30a516ce
RoboMWM <robomwm@users.noreply.github.com>
2017-08-21 17:13:57 -0700

    change formatting, fix crate give command, play a sound
    Sound not yet implemented at time of this commit

--- src/main/java/me/robomwm/MountainDewritoes/Rewards/LodsOfEmone.java
+++ src/main/java/me/robomwm/MountainDewritoes/Rewards/LodsOfEmone.java
@@ -3,6 +3,7 @@ package me.robomwm.MountainDewritoes.Rewards;
 import com.destroystokyo.paper.Title;
 import me.robomwm.MountainDewritoes.MountainDewritoes;
 import org.bukkit.ChatColor;
+import org.bukkit.SoundCategory;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.potion.PotionEffectType;
@@ -27,14 +28,14 @@ public class LodsOfEmone
         StringBuilder message = new StringBuilder();
         Title.Builder title = new Title.Builder();
         title.fadeIn(5);
-        title.fadeOut(60);
+        title.fadeOut(50);
         title.stay(60);
         switch (rewardType)
         {
             case XP_LEVELUP:
-                title.title(ChatColor.YELLOW + "                LEVEL UP!");
-                title.subtitle(ChatColor.WHITE + "                  lv" + level);
-                message.append(ChatColor.AQUA + ChatColor.BOLD.toString() + "Reached level " + level + "!" + ChatColor.DARK_AQUA + " U g0t:\n");
+                title.title(ChatColor.YELLOW + "            LEVEL UP!");
+                title.subtitle(ChatColor.WHITE + "                         Lv " + level);
+                message.append(ChatColor.AQUA + ChatColor.BOLD.toString() + "REACHED LEVEL " + level + "!" + ChatColor.DARK_AQUA + " U g0t:\n");
                 //Heal player
                 player.setHealth(player.getAttribute(Attribute.GENERIC_MAX_HEALTH).getValue());
                 player.removePotionEffect(PotionEffectType.POISON);
@@ -45,12 +46,11 @@ public class LodsOfEmone
                 message.append(instance.getEconomy().format(money) + "\n");
                 player.sendMessage(message.toString());
                 //Give random crate (needs to be updated as new crate series are added)
-                executeCommand("newcrate " + instance.r4nd0m(1, 5) + "1 " + player.getName());
+                executeCommand("newcrate " + String.valueOf(instance.r4nd0m(1, 5)) + " 1 " + player.getName());
                 //Also give key if level is divisible by 5
                 if (level % 5 == 0)
                     executeCommand("newkey 1 " + player.getName());
-                //TODO: sound
-
+                player.playSound(player.getLocation(), "fortress.levelup", SoundCategory.PLAYERS, 3000000f, 1.0f);
         }
         instance.getTitleManager().addUsingTitle(player, 10, title.build());
     }
