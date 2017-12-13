277f86a588f14e5597ad1771eaa9bbaf54426311
RoboMWM <robomwm@users.noreply.github.com>
2017-12-12 19:27:32 -0800

    wow an error message

--- src/main/java/me/robomwm/MountainDewritoes/TheMidnightPortalToAnywhere.java
+++ src/main/java/me/robomwm/MountainDewritoes/TheMidnightPortalToAnywhere.java
@@ -61,6 +61,7 @@ public class TheMidnightPortalToAnywhere implements Listener
         }
         catch (Throwable rock)
         {
+            instance.getLogger().warning("Could not load portals.yml.");
             return;
         }
 
