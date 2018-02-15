dc7a4674711b17eabe4bc7700ccd5c6d0b3d4467
RoboMWM <robomwm@users.noreply.github.com>
2018-02-15 15:20:54 -0800

    does spigot cancel at low or lowest? 🤔

--- src/main/java/me/robomwm/MountainDewritoes/armor/OldFood.java
+++ src/main/java/me/robomwm/MountainDewritoes/armor/OldFood.java
@@ -4,6 +4,7 @@ import org.bukkit.ChatColor;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
@@ -26,7 +27,7 @@ public class OldFood implements Listener
         instance = plugin;
     }
 
-    @EventHandler(ignoreCancelled = true)
+    @EventHandler(ignoreCancelled = true, priority = EventPriority.LOW)
     private void onPlayerPreEat(PlayerInteractEvent event)
     {
         Player player = event.getPlayer();
