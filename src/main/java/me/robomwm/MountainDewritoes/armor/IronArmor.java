95d84afa102a713e7ff763af57ce7999d3cbbd18
RoboMWM <robomwm@users.noreply.github.com>
2018-09-03 11:41:52 -0700

    If at 0 power, provide a "descending" hover

--- src/main/java/me/robomwm/MountainDewritoes/armor/IronArmor.java
+++ src/main/java/me/robomwm/MountainDewritoes/armor/IronArmor.java
@@ -60,49 +60,48 @@ public class IronArmor implements Listener
         if (!armorAugmentation.isEquipped(player, Material.IRON_BOOTS))
             return;
 
-        if (player.getFoodLevel() > 0)
+        BukkitRunnable runnable = new BukkitRunnable()
         {
-            BukkitRunnable runnable = new BukkitRunnable()
+            @Override
+            public void run()
             {
-                @Override
-                public void run()
+                if (!armorAugmentation.isEquipped(player, Material.IRON_BOOTS) || player.isOnGround())
                 {
-                    if (!armorAugmentation.isEquipped(player, Material.IRON_BOOTS) || player.isOnGround())
-                    {
-                        cancel();
-                        return;
-                    }
-                    //final int velocity = 1 + player.getFoodLevel() / 6;
-                    final int velocity = 1;
-                    //Compensate for falling velocity //No longer needed since we reduced power cost.
-//                    int velocity = (int)(-player.getVelocity().getY() * 15);
-//                    if (velocity < 1)
-//                        velocity = 1;
-//                    else if (velocity > 100)
-//                        velocity = 100;
-                    player.removePotionEffect(PotionEffectType.LEVITATION);
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 30, velocity, true, false));
-                    player.setFoodLevel(player.getFoodLevel() - 1);
+                    cancel();
+                    return;
                 }
+                //final int velocity = 1 + player.getFoodLevel() / 6;
+                int velocity = 1;
 
-                @Override
-                public synchronized void cancel() throws IllegalStateException
+                if (player.getFoodLevel() <= 1)
                 {
-                    super.cancel();
-                    player.removePotionEffect(PotionEffectType.LEVITATION);
-                    floaters.remove(player);
-                    try
-                    {
-                        if (ProtocolSupportAPI.getProtocolVersion(player).isBefore(ProtocolVersion.MINECRAFT_1_9))
-                            return;
-                    }
-                    catch (Throwable ignored){}
-                    player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 10, 250, true, false));
+                    velocity = 254;
+                    player.setFoodLevel(0);
                 }
-            };
-            floaters.put(player, runnable);
-            runnable.runTaskTimer(instance, 0L, 10L);
-        }
+                else
+                    player.setFoodLevel(player.getFoodLevel() - 1);
+
+                player.removePotionEffect(PotionEffectType.LEVITATION);
+                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 30, velocity, true, false));
+            }
+
+            @Override
+            public synchronized void cancel() throws IllegalStateException
+            {
+                super.cancel();
+                player.removePotionEffect(PotionEffectType.LEVITATION);
+                floaters.remove(player);
+                try
+                {
+                    if (ProtocolSupportAPI.getProtocolVersion(player).isBefore(ProtocolVersion.MINECRAFT_1_9))
+                        return;
+                }
+                catch (Throwable ignored){}
+                player.addPotionEffect(new PotionEffect(PotionEffectType.LEVITATION, 10, 250, true, false));
+            }
+        };
+        floaters.put(player, runnable);
+        runnable.runTaskTimer(instance, 0L, 10L);
     }
 
     @EventHandler(ignoreCancelled = true)
@@ -136,3 +135,10 @@ public class IronArmor implements Listener
         }.runTaskTimer(armorAugmentation.getPlugin(), 1L, 1L);
     }
 }
+
+//Compensate for falling velocity //No longer needed since we reduced power cost.
+//                    int velocity = (int)(-player.getVelocity().getY() * 15);
+//                    if (velocity < 1)
+//                        velocity = 1;
+//                    else if (velocity > 100)
+//                        velocity = 100;
