57befea7360e2faf2deb05be1d3345f3ff9dcccf
RoboMWM <robomwm@users.noreply.github.com>
2018-11-17 13:53:53 -0800

    wait 4 lax 2 fix his event

--- src/main/java/me/robomwm/MountainDewritoes/AntiLag.java
+++ src/main/java/me/robomwm/MountainDewritoes/AntiLag.java
@@ -1,6 +1,5 @@
 package me.robomwm.MountainDewritoes;
 
-import net.awesomepowered.rotator.event.RotatorSpinEvent;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
@@ -90,9 +89,9 @@ public class AntiLag implements Listener
         }
     }
 
-    @EventHandler(ignoreCancelled = true)
-    private void onlySpinForPpl(RotatorSpinEvent event)
-    {
-        event.setCancelled(event.getRotator().getLocation().getWorld().getPlayers().isEmpty());
-    }
+//    @EventHandler(ignoreCancelled = true)
+//    private void onlySpinForPpl(RotatorSpinEvent event)
+//    {
+//        event.setCancelled(event.getRotator().getLocation().getWorld().getPlayers().isEmpty());
+//    }
 }
